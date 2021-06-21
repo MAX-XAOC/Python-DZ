1bfec5d36487d12a992111fe3b96d860ec866c37
Maxim Kravchenko <traderxaoc@yandex.ru>
2021-06-21 04:17:51 +0300

    3 dz

--- 3.py
+++
@@ -1,9 +0,0 @@
-def my_func(arg_1, arg_2, arg_3):
-    spisok = [arg_1, arg_2, arg_3]
-    try:
-        spisok.remove(min(spisok))
-        return sum(spisok)
-    except TypeError:
-        return "Введите только числа"
-
-print(my_func(1, 2, 3))
--- 5.py
+++
@@ -1,8 +0,0 @@
-num = 0
-try:
-    while num != 'A':
-        for i in map(int, input(' Введите числа  используя пробел (Для выхода введите A):  ').split()):
-            num += i
-        print(num)
-except ValueError:
-    print(num)
